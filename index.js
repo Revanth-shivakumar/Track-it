d62dd160eae7809c485ed8a42be26f8352423aa6
rv <aaa@aa.com>
2023-02-10 11:34:28 +0530

    basic js files

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express=require('express');
 const dbConnect=require('./config/db.js');
 const cors=require('cors');
 const app=express();
+app.use(cors());
+dbConnect();
+app.use(express.json());
+
+// port
+const PORT=process.env.PORT || 6001;
+app.listen(PORT,()=>console.log(`Server up at port ${PORT}`));
